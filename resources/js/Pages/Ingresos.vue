ff3b91badb5714e11e7ab52816365a208f914609
CHRC <cromero@dannyyesoft.mx>
2022-04-08 13:32:35 -0500

    Gráficos

--- resources/js/Pages/Ingresos.vue
+++ resources/js/Pages/Ingresos.vue
@@ -3,13 +3,23 @@
         <template #header>
             <h1 class="text-center text-2xl font-bold leading-7 text-gray-300 sm:text-3xl sm:truncate py-4 bg-gradient-to-l from-indigo-500 to-indigo-800 ">
                 Ingreso diario por Estacionamiento</h1>
-                {{ingresos}}
-        </template>
-       <template>
-    <div>
-        <Bar />
-    </div>
-</template>
+              
+                </template>
+                    {{ingresos}}
+                
+                <template>
+                    <Bar
+                        :chart-options="chartOptions"
+                        :chart-data="chartData"
+                        :chart-id="chartId"
+                        :dataset-id-key="datasetIdKey"
+                        :plugins="plugins"
+                        :css-classes="cssClasses"
+                        :styles="styles"
+                        :width="width"
+                        :height="height"
+                    />
+                </template>
 </app-layout>
 </template>
 
@@ -17,34 +27,71 @@
 import AppLayout from "@/Layouts/AppLayout";
 import { Link } from '@inertiajs/inertia-vue3';
 import { Bar, Line  } from 'vue-chartjs';
-import { shuffle } from 'lodash';
-import { computed, defineComponent, ref } from 'vue';
-import { DoughnutChart } from 'vue-chart-3';
-import VueCharts from 'vue-chartjs';
-import { Chart, DoughnutController, ArcElement, Tooltip } from 'chart.js';
-Chart.register(DoughnutController, ArcElement, Tooltip);
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default ({
-  
+  name: 'BarChart',
   components: { 
-      DoughnutChart,
       AppLayout,
-      Bar
+      Bar,
+      Link
       },
         props:{
         ingresos: Array,
+        chartId: {
+      type: String,
+      default: 'bar-chart'
     },
-   extends: Bar,
-  mounted () {
-    // Overwriting base render method with actual data.
-    this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      datasets: [
-        {
-          label: 'GitHub',
-          backgroundColor: '#f87979',
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-        }
-      ]
-    })}
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {}
+    },
+    plugins: {
+      type: Object,
+      default: () => {}
+    },
+     data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+
+    }
 });
 </script>
+
+<style type="text/css">
+
+    #Grafico {
+        z-index: 800;
+        height: 70px;
+        border: 1px dashed #999966;
+        background-color: #ffffcc;
+        margin: 0px 50px 0px 50px;
+        text-align: center;
+    }  
+    
+
+    </style>
